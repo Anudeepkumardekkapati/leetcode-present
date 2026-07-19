612aa1add3c3c25fa0ab21981ba48bafb911199b
Anudeep Kumar <24a95a1202@aec.edu.in>
2026-07-19 21:23:36 +0530

    Time: 11 ms (76.35%), Space: 19.3 MB (29.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,9 +4,7 @@ class Solution:
         maxi=0
         sets=set()
         left=0
-        if set(s)==1:
-            
-            return len(s)-1
+        
         for i in range(len(s)):
             if s[i] not in sets:
                 sets.add(s[i])
