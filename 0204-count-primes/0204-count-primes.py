9a5e5ed2b5a8c13cdeb7893331e7f5b5e57fefec
Anudeep Kumar <24a95a1202@aec.edu.in>
2026-07-25 20:00:00 +0530

    Time: 1729 ms (31.09%), Space: 58.1 MB (43.14%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<2:
+            return 0 
+     
+        emt=[True]*n
+
+        emt[0]=False
+        emt[1]=False
+
+        for i in range(2,n):
+            if emt[i]:
+                for j in range(i*i,n,i):
+                    emt[j]=False
+
+        return sum(emt)
+            
+        
