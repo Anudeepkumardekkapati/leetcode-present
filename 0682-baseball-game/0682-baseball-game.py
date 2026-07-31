0732e343066a0a89a51edff55602faf32beeb951
Anudeep Kumar <24a95a1202@aec.edu.in>
2026-07-31 22:17:32 +0530

    Time: 0 ms (100%), Space: 19.7 MB (12.13%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,18 +1,20 @@
 class Solution:
     def calPoints(self, nums: List[str]) -> int:
-        st=[]
-        for i in range(len(nums)):
-            if nums[i]=="C":
-                st.pop()
-            elif  nums[i]=="D":
-                val=int(st[-1])*2
-                st.append(val)
-            elif   nums[i]=="+":
-                vals=int(st[-1])+int(st[-2])
-                st.append(vals)
 
+        st=[]
+        for i in nums:
+            if st and i=="+":
+                first=int(st[-1])
+                second=int(st[-2])
+                st.append(first+second)
+            
+            elif st and i=="D":
+                prev=int(st[-1])*2
+                st.append(prev)
+            elif st and i=="C":
+                st.pop()
             else:
-                st.append(int(nums[i]))
-        return(sum(st))
+                st.append(int(i))
+        return sum(st)
 
         
