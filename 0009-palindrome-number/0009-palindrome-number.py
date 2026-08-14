700946be74be5720dff54f646655fea1ea5efcb6
Anudeep Kumar <24a95a1202@aec.edu.in>
2026-08-14 23:39:16 +0530

    Time: 13 ms (23.21%), Space: 19.2 MB (86.8%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s=str(x)
+        return self.check(s,0,len(s)-1)
+    
+
+    def check(self,s,left,right):
+        if left>=right:
+            return True
+        if s[left]!=s[right]:
+            return False
+        return self.check(s,left+1,right-1)
+        
