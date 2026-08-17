f6623fe0913fe11fbba85953c4955e22c0df891c
Anudeep Kumar <24a95a1202@aec.edu.in>
2026-08-17 11:09:32 +0530

    Time: 3 ms (79.64%), Space: 20.6 MB (8.02%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l=0
+        r=len(nums)-1
+        while l<r:
+            val=nums[l]+nums[r]
+            if val==target:
+                return [l+1,r+1]
+            elif val<target:
+                l+=1
+            else:
+                r-=1
+           
+            
+
+        
